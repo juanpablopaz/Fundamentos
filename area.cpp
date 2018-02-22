e6cff656d397d94ca045460568c7ee9c3402bfcf
juanpablopaz <36507241+juanpablopaz@users.noreply.github.com>
2018-02-22 09:12:36 -0400

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,22 @@
+// area1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+void main () 
+{
+	float area, base, altura;
+	cout << "ingrese base";
+	cin >> base;
+	cout << "ingrese altura";
+	cin >> altura;
+	area= (base*altura)/2;
+	cout << "el area es " << area << endl;
+	getch ();
+	
+}
+
